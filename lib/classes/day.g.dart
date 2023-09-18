a4c1d81586adaa6a7a1f37b103cdb484b5a12250
aevinj <ajjaevinjais@gmail.com>
2023-09-18 22:59:20 +0100

    hive type adapters generated

---
+++ lib/classes/day.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'day.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class DayAdapter extends TypeAdapter<Day> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Day read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Day(
+      dayID: fields[0] as String,
+      exercises: (fields[1] as List).cast<Exercise>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Day obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.dayID)
+      ..writeByte(1)
+      ..write(obj.exercises);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DayAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
