1f1a6273a7f1c148d4047c010e845e3761ba44dd
aevinj <ajjaevinjais@gmail.com>
2023-09-20 17:59:48 +0100

    prebuilt exercise UI

--- lib/pages/add_exercise.dart
+++ lib/pages/add_exercise.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:progressive_overload/classes/exercise.dart';
@@ -38,63 +39,101 @@ class _AddExerciseState extends State<AddExercise> {
             style: Font(),
           )),
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        TextField(
-          style: const TextStyle(color: Colors.white),
-          controller: _nameController,
-          decoration: InputDecoration(
-            hintText: 'Exercise name',
-            hintStyle: TextStyle(color: Colors.grey[600]),
-            focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey[800]!),
-            ),
-          ),
-        ),
+        BlurryButton(
+            width: 200,
+            height: 200,
+            onPressed: () {},
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  CupertinoIcons.add,
+                  color: Colors.white,
+                  size: 75,
+                ),
+                Text(
+                  "Choose an exercise",
+                  style: Font(),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            )),
         const SizedBox(
           height: 30,
         ),
-        TextField(
-          style: const TextStyle(color: Colors.white),
-          controller: _repsController,
-          keyboardType: TextInputType.number,
-          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-          decoration: InputDecoration(
-            hintText: 'Reps',
-            hintStyle: TextStyle(color: Colors.grey[600]),
-            focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey[800]!),
-            ),
-          ),
-        ),
+        Row(children: [
+          Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                "Custom exercise:",
+                style: Font(size: 22),
+              ))
+        ]),
         const SizedBox(
           height: 30,
         ),
-        TextField(
-          style: const TextStyle(color: Colors.white),
-          controller: _setsController,
-          keyboardType: TextInputType.number,
-          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-          decoration: InputDecoration(
-            hintText: 'Sets',
-            hintStyle: TextStyle(color: Colors.grey[600]),
-            focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(children: [
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: 'Exercise name',
+                hintStyle: TextStyle(color: Colors.grey[600]),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey[800]!),
+                ),
+              ),
             ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey[800]!),
+            const SizedBox(
+              height: 30,
             ),
-          ),
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              controller: _repsController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: InputDecoration(
+                hintText: 'Reps',
+                hintStyle: TextStyle(color: Colors.grey[600]),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey[800]!),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              controller: _setsController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: InputDecoration(
+                hintText: 'Sets',
+                hintStyle: TextStyle(color: Colors.grey[600]),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey[800]!),
+                ),
+              ),
+            ),
+          ]),
         ),
         const SizedBox(
-          height: 50,
+          height: 25,
         ),
         BlurryButton(
-            height: 100,
+            height: 75,
             width: 150,
             onPressed: () {
               final newExercise = Exercise(
