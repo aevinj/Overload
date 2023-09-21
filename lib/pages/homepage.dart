b8d0eeb6a04b968540ae351b7d795d15d03bff66
aevinj <ajjaevinjais@gmail.com>
2023-09-22 00:46:31 +0100

    clearing box uses manager class

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:progressive_overload/classes/workout.dart';
+import 'package:progressive_overload/box_manager.dart';
 import 'package:progressive_overload/components/blurred_button.dart';
 import 'package:progressive_overload/components/text_style.dart';
 import 'package:progressive_overload/pages/new_workout.dart';
 import 'package:progressive_overload/pages/view_workouts.dart';
 import 'package:progressive_overload/theme/dark_theme.dart';
+import 'package:provider/provider.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -18,6 +18,8 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
+    final boxManager = Provider.of<BoxManager>(context);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text("Home", style: Font()),
@@ -62,11 +64,7 @@ class _HomepageState extends State<Homepage> {
           ),
           BlurryButton(
             onPressed: () async {
-              final box = await Hive.openBox<Workout>(
-                  "workouts");
-              await box.clear();
-
-              await box.close();
+              await boxManager.clearBox();
               print("done");
             },
             width: 250,
