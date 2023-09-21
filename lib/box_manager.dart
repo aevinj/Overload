c90043c2671b7da7de66c54c76ca534488d4af99
aevinj <ajjaevinjais@gmail.com>
2023-09-21 23:27:05 +0100

    delete day and delete exercise methods

--- lib/box_manager.dart
+++ lib/box_manager.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:hive/hive.dart';
+import 'package:progressive_overload/classes/exercise.dart';
 import 'package:progressive_overload/classes/workout.dart';
 
 class BoxManager extends ChangeNotifier {
@@ -23,5 +24,39 @@ class BoxManager extends ChangeNotifier {
 
   Future<void> deleteAtIndex(int index) async {
     await box.deleteAt(index);
+    notifyListeners();
+  }
+
+  void deleteExercise(Exercise exercise, int workoutIndex, String day) {
+    var newWorkout = box.getAt(workoutIndex)!;
+    newWorkout.days[_convertDayToDayIndex(day)].exercises.remove(exercise);
+    box.putAt(workoutIndex, newWorkout);
+    notifyListeners();
+  }
+
+  void deleteDay(int workoutIndex, String day) {
+    var newWorkout = box.getAt(workoutIndex)!;
+    newWorkout.days.removeAt(_convertDayToDayIndex(day));
+    box.putAt(workoutIndex, newWorkout);
+    notifyListeners();
+  }
+
+  int _convertDayToDayIndex(String day) {
+    if (day == "Monday") {
+      return 0;
+    } else if (day == "Tuesday") {
+      return 1;
+    } else if (day == "Wednesday") {
+      return 2;
+    } else if (day == "Thursday") {
+      return 3;
+    } else if (day == "Friday") {
+      return 4;
+    } else if (day == "Saturday") {
+      return 5;
+    } else if (day == "Sunday") {
+      return 6;
+    }
+    return -1;
   }
 }
