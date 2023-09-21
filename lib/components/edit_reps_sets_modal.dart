5d6b660d4d107c0b5b03a89ce7615d3915d67cf9
aevinj <ajjaevinjais@gmail.com>
2023-09-22 00:32:32 +0100

    new modal sheet for editing reps and sets

---
+++ lib/components/edit_reps_sets_modal.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:progressive_overload/components/blurred_button.dart';
+import 'package:progressive_overload/components/text_style.dart';
+
+class ModalSheetContent extends StatefulWidget {
+  final int initialSetCount;
+  final Function(int) onSetCountChanged;
+  final int initialRepsCount;
+  final Function(int) onRepsCountChanged;
+
+  const ModalSheetContent(
+      {required this.initialSetCount,
+      required this.onSetCountChanged,
+      required this.initialRepsCount,
+      required this.onRepsCountChanged});
+
+  @override
+  _ModalSheetContentState createState() => _ModalSheetContentState();
+}
+
+class _ModalSheetContentState extends State<ModalSheetContent> {
+  late int setCount;
+  late int repCount;
+
+  @override
+  void initState() {
+    super.initState();
+    setCount = widget.initialSetCount;
+    repCount = widget.initialRepsCount;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              BlurryButton(
+                width: 50,
+                height: 50,
+                onPressed: () {
+                  setState(() {
+                    // Decrease repCount by 1
+                    repCount--;
+                    widget.onRepsCountChanged(repCount);
+                  });
+                },
+                child: const Icon(
+                  CupertinoIcons.minus,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "$repCount reps",
+                style: Font(),
+              ),
+              BlurryButton(
+                width: 50,
+                height: 50,
+                onPressed: () {
+                  setState(() {
+                    // Increase repCount by 1
+                    repCount++;
+                    widget.onRepsCountChanged(repCount);
+                  });
+                },
+                child: const Icon(
+                  CupertinoIcons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              BlurryButton(
+                width: 50,
+                height: 50,
+                onPressed: () {
+                  setState(() {
+                    // Decrease setCount by 1
+                    setCount--;
+                    widget.onSetCountChanged(setCount);
+                  });
+                },
+                child: const Icon(
+                  CupertinoIcons.minus,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "$setCount sets",
+                style: Font(),
+              ),
+              BlurryButton(
+                width: 50,
+                height: 50,
+                onPressed: () {
+                  setState(() {
+                    // Increase setCount by 1
+                    setCount++;
+                    widget.onSetCountChanged(setCount);
+                  });
+                },
+                child: const Icon(
+                  CupertinoIcons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
