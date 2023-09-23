100c7b690eb32dc5ecf64ba14eb657739d596eeb
aevinj <ajjaevinjais@gmail.com>
2023-09-23 21:14:12 +0100

    create new exercise cell component

---
+++ lib/components/exercise_category_cell.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:progressive_overload/components/blurred_button.dart';
+import 'package:progressive_overload/components/text_style.dart';
+
+class ExerciseCategoryCell extends StatelessWidget {
+  final String imgPath;
+  final String categoryName;
+
+  const ExerciseCategoryCell(
+      {super.key, required this.categoryName, required this.imgPath});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlurryButton(
+        height: 200,
+        width: 200,
+        onPressed: () {},
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image(
+                image: AssetImage(imgPath),
+                width: 150,
+                height: 125,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              categoryName,
+              style: Font(),
+            )
+          ],
+        ));
+  }
+}
