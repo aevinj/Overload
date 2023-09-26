55f1b9a0079b406742a840d3f644338333167f83
aevinj <ajjaevinjais@gmail.com>
2023-09-26 23:06:26 +0100

    resizabillity for add exercise screen

--- lib/pages/add_exercise.dart
+++ lib/pages/add_exercise.dart
@@ -7,6 +7,7 @@ import 'package:progressive_overload/components/capitalise.dart';
 import 'package:progressive_overload/components/text_style.dart';
 import 'package:progressive_overload/pages/exercise_chooser.dart';
 import 'package:progressive_overload/theme/dark_theme.dart';
+import 'package:progressive_overload/util/device_specific.dart';
 
 class AddExercise extends StatefulWidget {
   const AddExercise({super.key});
@@ -34,21 +35,31 @@ class _AddExerciseState extends State<AddExercise> {
       resizeToAvoidBottomInset: false,
       backgroundColor: darkBackground(),
       appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: Text(
-            "New exercise",
-            style: Font(),
-          )),
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        leadingWidth: 20,
+        iconTheme: const IconThemeData(size: 30, color: Colors.white),
+        title: Row(children: [
+          const Spacer(),
+          Text(
+            "Create an exercise",
+            style: Font(size: 30),
+          ),
+          const Spacer(),
+          const SizedBox(
+            width: 20,
+          )
+        ]),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Column(children: [
+        const Spacer(),
         BlurryButton(
-            width: 200,
-            height: 200,
+            width: widthOfCurrentDevice(context) * 0.4,
+            height: heightOfCurrentDevice(context) * 0.25,
             onPressed: () {
               Navigator.push(
                 context,
                 CupertinoPageRoute(
-                  builder: (context) => const ExerciseChooser()
-                ),
+                    builder: (context) => const ExerciseChooser()),
               );
             },
             child: Column(
@@ -66,58 +77,53 @@ class _AddExerciseState extends State<AddExercise> {
                 )
               ],
             )),
-        const SizedBox(
-          height: 30,
-        ),
+        const Spacer(),
         Row(children: [
           Padding(
-              padding: const EdgeInsets.only(left: 10),
+              padding: const EdgeInsets.only(left: 16),
               child: Text(
                 "Custom exercise:",
                 style: Font(size: 22),
               ))
         ]),
-        const SizedBox(
-          height: 30,
-        ),
         Padding(
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(16),
           child: Column(children: [
-            TextField(
-              style: const TextStyle(color: Colors.white),
-              controller: _nameController,
-              decoration: InputDecoration(
-                hintText: 'Exercise name',
-                hintStyle: TextStyle(color: Colors.grey[600]),
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey[800]!),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            TextField(
-              style: const TextStyle(color: Colors.white),
-              controller: _repsController,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: InputDecoration(
-                hintText: 'Reps',
-                hintStyle: TextStyle(color: Colors.grey[600]),
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey[800]!),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5),
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                controller: _nameController,
+                decoration: InputDecoration(
+                  hintText: 'Exercise name',
+                  hintStyle: TextStyle(color: Colors.grey[600]),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[800]!),
+                  ),
                 ),
               ),
             ),
-            const SizedBox(
-              height: 30,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5),
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                controller: _repsController,
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: InputDecoration(
+                  hintText: 'Reps',
+                  hintStyle: TextStyle(color: Colors.grey[600]),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[800]!),
+                  ),
+                ),
+              ),
             ),
             TextField(
               style: const TextStyle(color: Colors.white),
@@ -137,12 +143,10 @@ class _AddExerciseState extends State<AddExercise> {
             ),
           ]),
         ),
-        const SizedBox(
-          height: 25,
-        ),
+        const Spacer(),
         BlurryButton(
-            height: 75,
-            width: 150,
+            height: heightOfCurrentDevice(context) * 0.125,
+            width: widthOfCurrentDevice(context) * 0.8,
             onPressed: () {
               if (_nameController.text.isNotEmpty) {
                 final newExercise = Exercise(
@@ -168,7 +172,8 @@ class _AddExerciseState extends State<AddExercise> {
             child: Text(
               "Add",
               style: Font(),
-            ))
+            )),
+        const Spacer(),
       ]),
     );
   }
