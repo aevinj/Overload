f909d16eea58a5baef148b204632dedf6b062f95
aevinj <ajjaevinjais@gmail.com>
2023-09-29 00:07:04 +0100

    new font

--- lib/components/text_style.dart
+++ lib/components/text_style.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 TextStyle Font({ double size = 25.0, Color color = Colors.white, bool bold = false}) {
-  return TextStyle(fontSize: size, color: color, fontWeight: bold ? FontWeight.bold : FontWeight.normal);
+  return TextStyle(fontSize: size, color: color, fontWeight: bold ? FontWeight.bold : FontWeight.normal, fontFamily: 'SFPro');
 }
